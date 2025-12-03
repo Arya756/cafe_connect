f8415edfa5ea9d95e347d4c89d1fecbf81201fb0
Arya756 <ayusharyan1709@gmail.com>
2025-12-03 11:48:35 +0530

    Fix: Add SSL/TLS support for Aiven MySQL connection

--- server/config/database.js
+++ server/config/database.js
@@ -7,10 +7,17 @@ const sequelize = new Sequelize({
     dialect: process.env.DB_DIALECT || 'sqlite',
     storage: process.env.DB_DIALECT === 'sqlite' ? './database.sqlite' : undefined,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     database: process.env.DB_NAME,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     logging: false,
+    dialectOptions: process.env.DB_DIALECT === 'mysql' ? {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    } : {}
 });
 
 module.exports = sequelize;
